a2d015a8b841e7ffbb73232237bf6e70e8977405
Mohammad8931 <mohammadmasarra27@gmail.com>
2025-06-02 17:52:06 +0200

    trained RandomForest model, R²=50% on test, signs of overfitting, no underfitting, i will try some regularization techniques

--- model_train.py
+++ model_train.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+import time 
+from sklearn.metrics import mean_absolute_error, r2_score, root_mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
 
 def prepare_features_target(df):
     print(df.columns)
@@ -20,6 +22,38 @@ def split_data(X, y, test_size=0.2, random_state=42):
     
     return X_train, X_test, y_train, y_test
 
+def train_and_evaluate_model(model, X_train, X_test, y_train, y_test, model_name="Model"):
+
+    # Train the model
+    model.fit(X_train, y_train)
+    # Make predictions
+    y_test_pred = model.predict(X_test)
+
+    mae = mean_absolute_error(y_test, y_test_pred)
+    
+    r2_test = r2_score(y_test, y_test_pred)
+    
+    RMSE = root_mean_squared_error(y_test, y_test_pred)
+    
+    
+    # i will computer training error and compare it with test error to check underfitting and overfitting
+    y_train_pred = model.predict(X_train)
+    r2_train = r2_score(y_train, y_train_pred)
+
+    print(f"its MAE: {mae}, RMSE : {RMSE}, R² score: {r2_test}")
+    print(f"Train R²: {r2_train}, Test R²: {r2_test}")
+
+    return model
+
+
+
+
 dataset = pd.read_csv('dataset/preprocessed_dataset.csv')
 X, y = prepare_features_target(dataset)
 X_train, X_test, y_train, y_test = split_data(X,y)
+model = train_and_evaluate_model(RandomForestRegressor(n_estimators=300, max_depth=10,  min_samples_split=5, min_samples_leaf=2,random_state=42))
+
+
+
+
+
